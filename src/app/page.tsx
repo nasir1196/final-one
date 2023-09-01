d5fa25e37684f043ba2257d4fec566ef73052f82
nasir1196 <nasiruddin.cf@gmail.com>
2023-09-01 18:09:11 +0600

    code replace & refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-
-import Head from 'next/head';
+"use client"
 import Homes from "@/components/homePage/Homes/Homes";
-
 
 
 export default function Home( ) {
 
     return (
-    <>
-        <Head>
-            <title>One-Call-kuwait-Home</title>
-            <meta name="description" content="home touch maintainance" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-
         <main>
             <Homes/>
         </main>
 
-    </>
   )
 }
