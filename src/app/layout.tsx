7893d76ad91c4627a5c3eb1c0024b875632cdc9d
nasir1196 <nasiruddin.cf@gmail.com>
2023-09-01 18:17:17 +0600

    code replace & refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 
 const inter = Inter({subsets: ['latin']})
@@ -15,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: { children: React.ReactNode }) {
     return (
         <html lang="en">
+        <Head>
+            <title>One Call Kuwait</title>
+        </Head>
         <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar/>
         <div>
